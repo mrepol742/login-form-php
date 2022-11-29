947f6788b2a15208b5161a943cc29f01200f4d85
mrepol742 <mrepol742@gmail.com>
2022-11-29 18:23:12 +0800

    Initial Commit

--- create.php
+++ create.php
@@ -1,98 +1,87 @@
 <?php
-include("connections.php");
-
-$fullname = $email = $password = $cpassword = $success = "";
+$fullname = $email = $password = $cpassword = "";
 $fullnameErr = $emailErr = $passwordErr = $cpasswordErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
  if (empty($_POST["fullname"])) {
 	 $fullnameErr = "Fullname is required!";
-     $success = "";
  } else {
 	 $fullname = $_POST["fullname"];
  }
  if (empty($_POST["email"])) {
 	 $emailErr = "Email is required!";
-     $success = "";
  } else {
 	 $email = $_POST["email"];
  }
  if (empty($_POST["password"])) {
 	 $passwordErr = "Password is required!";
-     $success = "";
  } else {
 	 $password = $_POST["password"];
  }
  if (empty($_POST["cpassword"])) {
 	 $cpasswordErr = "Confirm Password is required!";
-     $success = "";
  } else {
 	 $cpassword = $_POST["cpassword"];
  }
  
+ 
+ 
+ 
+ 
  if (isset($_POST['loginB'])) {
      header('Location: login.php');
  }
- if (isset($_POST['submit'])) {
-    if (!empty($fullname) && !empty($email) && !empty($password)) {
-    $sql = "INSERT INTO thecompany (fullname, email, pass)
-    VALUES ('" . $fullname . "', '" . $email . "', '" . $password . "')";
-    
-    if ($conn->query($sql) === TRUE) {
-        $success = "Successful.";
-    }
-    }
- }
- 
 }
 
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
+
+        <meta name="hostname" content="mrepol742.github.io">
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta name="theme-color" content="#ffffff">
-        <link rel="stylesheet" href="assets/bootstrap-5.0.2css.min.css">
+        <link rel="stylesheet" href="assets/bootstrap.min.css">
         <link rel="stylesheet" href="css/main.css">
+		    <title>Create Account</title>
 
-        <link rel="shortcut icon" href="img/bestlinkcollegeofthephilippines.png">
-        <link rel="apple-touch-icon" href="img/bestlinkcollegeofthephilippines.png">
-		<title>Create Account</title>
     </head>
     <body>
-        <div class="cursor"></div>
         <div class="container">
             <div class="cover">
             <img src="img/bestlinkcollegeofthephilippines.png" alt="Bestlink College of the Philippines" width="100" class="icon">
-            <h1 id="login">Create Account</h1>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-                <?php echo "<div class=\"success\">".$success."</div>" ?>
-                <label class="input" for="fullname">Fullname:</label> <br>
-                <input id="fullname" placeholder="John Doe" type="text" name="fullname">
-                <?php echo "<div class=\"err\">".$fullnameErr."</div>" ?>
-                <label class="input" for="email">Email:</label> <br> 
-                <input id="email" placeholder="something@gmail.com" type="email" name="email">
-                <?php echo "<div class=\"err\">".$emailErr."</div>" ?> 
-                <label class="input" for="password">Password:</label> <br> 
-                <input id="password" placeholder="**********" type="password" name="password">
-                <?php echo "<div class=\"err\">".$passwordErr."</div>" ?> 
-                <label class="input" for="cpassword">Confirm Password:</label> <br>
-                <input id="cpassword" placeholder="**********" type="password" name="cpassword">
-                <?php echo "<div class=\"err\">".$cpasswordErr."</div>" ?>
-                <br>
-                <button id="createNewAccount" type="submit" name="submit">Create Account</button>
+            <h1 id="login">Signup</h1>
+            <form action="<?php htmlspecialchars('php_self'); ?>" method="post">
+                <?php if (isset($_GET['error'])) { ?>
+                    <p style="float: left;">
+                        <?php echo $_GET['error']; ?>
+                    </p>
+                <?php } ?>
+                <label for="fullname">Fullname:</label> <br>
+                <input id="fullname" placeholder="" type="text"> <br>
+                <label for="email">Email:</label> <br>
+                <input id="email" placeholder="something@gmail.com" type="email"> <br>
+                <label for="password">Password:</label> <br>
+                <input id="password" placeholder="**********" type="password"> <br>
+                <label for="cpassword">Confirm Password:</label> <br>
+                <input id="cpassword" placeholder="**********" type="password"> <br>
+            <br>
+                <button id="createNewAccount" type="submit">Create Account</button>
+               
                 <button id="loginB" name="loginB">Login</button>
             </form>
             </div>
         </div>
 
-        <script src="assets/bootstrap-5.0.2js.min.js"></script>
-        <script src="assets/lozad.min.js"></script>
+        <script src="assets/bootstrap.min.js"></script>
+        <script src="assets/typed.js"></script>
         <script src="js/main.js"></script>
+
     </body>
 </html>
--- login.php
+++ login.php
@@ -1,60 +1,26 @@
 <?php
-include("connections.php");
-
-$email = $password = $success = "";
-$emailErr = $passwordErr = "";
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
- 
-     if (empty($_POST["email"])) {
-         $emailErr = "Email is required!";
-     } else {
-         $email = $_POST["email"];
-     }
-     if (empty($_POST["password"])) {
-         $passwordErr = "Password is required!";
-     } else {
-         $password = $_POST["password"];
-     }
-     
-    
+
     if (isset($_POST['createNewAccount'])) {
         header('Location: create.php');
     } else if (isset($_POST['forgotPassword'])) {
         header('Location: forgot-password.php');
-    } 
-	else if (isset($_POST['submit'])) {
-		   if (!empty($email) && !empty($password)) {
-	   $sql = "SELECT * FROM thecompany";
-      $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-		    while($row = $result->fetch_assoc()) {
-               if ($email == $row["email"] && $password == $row["pass"]) {
-				   $success = "";
-			     	 header('Location: index.php');
-			   } else {
-				    $success = "Failed to login!";
-			   }
-             }
-        }
-		   }
-	 }
+    }
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
     <head>
+
+        <meta name="hostname" content="mrepol742.github.io">
+
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <meta name="theme-color" content="#ffffff">
-        <link rel="stylesheet" href="assets/bootstrap-5.0.2css.min.css">
+        <link rel="stylesheet" href="assets/bootstrap.min.css">
         <link rel="stylesheet" href="css/main.css">
-
-        <link rel="shortcut icon" href="img/bestlinkcollegeofthephilippines.png">
-        <link rel="apple-touch-icon" href="img/bestlinkcollegeofthephilippines.png">
         <title>Login</title>
     </head>
     <body>
@@ -63,26 +29,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <img src="img/bestlinkcollegeofthephilippines.png" alt="Bestlink College of the Philippines" width="100" class="icon">
             <h1 id="login">Login</h1>
             <form action="<?php htmlspecialchars('php_self'); ?>" method="post">
-			<?php echo "<div class=\"err\">".$success."</div>" ?>
-                <label class="input" for="email">Email:</label> <br>
-                <input id="email" placeholder="something@gmail.com" type="email" name="email">
-                <?php echo "<div class=\"err\">".$emailErr."</div>" ?> 
-                <label class="input" for="password">Password:</label> <br>
-                <input id="password" placeholder="**********" type="password" name="password">
-                <?php echo "<div class=\"err\">".$passwordErr."</div>" ?> 
-                <br>
+                <?php if (isset($_GET['error'])) { ?>
+                    <p style="float: left;">
+                        <?php echo $_GET['error']; ?>
+                    </p>
+                <?php } ?>
+                <label for="email">Email:</label> <br>
+                <input id="email" placeholder="something@gmail.com" type="email"> <br>
+                <label for="password">Password:</label> <br>
+                <input id="password" placeholder="**********" type="password"> <br>
+                  <br>
                 <button id="forgotPassword" name="forgotPassword">Forgot Password</button>
-                <button id="loginB" type="submit" name="submit">Login</button>
                 <hr>
                 <button id="createNewAccount" name="createNewAccount">Create New Account</button>
             </form>
             </div>
         </div>
 
-        <script src="assets/bootstrap-5.0.2js.min.js"></script>
-        <script src="assets/lozad.min.js"></script>
+        <script src="assets/bootstrap.min.js"></script>
+        <script src="assets/typed.js"></script>
         <script src="js/main.js"></script>
-
 
     </body>
 </html>
